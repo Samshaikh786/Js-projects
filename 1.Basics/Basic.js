5bf65a80e6eae61bc5164685f4ff363a0559b834
Samshaikh786 <112815420+Samshaikh786@users.noreply.github.com>
2022-12-27 20:14:09 -0800

    js-completed

---
+++ 1.Basics/Basic.js
@@ -0,0 +1,14 @@
+// Variables
+let name = "Mosh";
+console.log(name);
+
+// cannot be a reservered keyword
+// Should be Meaningful
+// Cannot start with a number
+// cannot contatin space or hyphen
+// Are case-sensetive
+
+
+// if wants to deckare multiple variables
+let firstName = "Mosh", lastName = "Hamedani";
+// but in modern practice  variables should be de calre separately
---
+++ 2.OPERATORS/JsPractise.js
@@ -0,0 +1,67 @@
+//Operators
+//1 == Arithmetic Operators
+let m = 10;
+let n = 2;
+// Addition  +
+let add = m + n;
+console.log(add)
+// Substraction -
+let subs = (m - n);
+console.log(subs);
+// Multiplication *
+let mult = (m * n)
+console.log(mult);
+// Division /
+let Divi = (m / n)
+console.log(Divi);
+// Modulus  %
+let mod = (m % 2)
+console.log(mod)
+// Exponential **(power)
+let expo = (m ** n)
+console.log(expo)
+// Increament ++
+let inc = (++m);
+console.log(inc);
+// decreament --
+let dec = (--n);
+console.log(dec)
+
+//2 == Assignment Oprator
+let p = 10;
+++p;
+// +=
+p += 2;
+console.log(p);
+// *=
+// -=
+// /=
+// %=
+
+// 3 == Comparison Operator
+// > greater
+// >= great eqaul
+// <
+// <= less equal 
+
+// 4 == Equality Operator
+// === strict Check (both value and type should be sam)
+// == Loose Check (only value should be equal)
+
+// 5 == Ternary Operator (starts with Condition)
+let point = 120;
+let type = point > 100 ? "gold" : "silver";
+console.log(type);
+
+
+// Swapping the Variables
+let x = "red";
+let y = "blue";
+
+let z = x;
+x = y;
+y = z;
+
+
+console.log(x);
+console.log(y);
